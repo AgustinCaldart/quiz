ee1181ed7c712ff89f119506cbe38a9e6f4399d9
AgustinCaldart <caldart123@gmail.com>
2021-09-26 11:22:09 -0300

    final

--- src/App.js
+++ src/App.js
@@ -3,39 +3,38 @@ import React, { useState } from 'react';
 export default function App() {
 	const questions = [
 		{
-			questionText: 'What is the of France?',
+			questionText: '¿Como se define un Channel de solo lectura?',
 			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
+				{ answerText: 'Rchan', isCorrect: false },
+				{ answerText: 'chan<-', isCorrect: false },
+				{ answerText: 'chan->', isCorrect: false },
+				{ answerText: '<-chan', isCorrect: true },
 			],
 		},
 		{
-			questionText: 'Who is CEO of Tesla?',
+			questionText: '¿Con base a que concepto se utilizan los Waitgroups?',
 			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
+				{ answerText: 'Contador', isCorrect: true },
+				{ answerText: 'Buffer', isCorrect: false },
+				{ answerText: 'Comunicacion', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'The iPhone was created by which company?',
+			questionText: '¿Cuando usariamos la concurrencia?',
 			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
+				{ answerText: 'Cuando una tarea se puede dividir en diferentes segmentos para funcionar mejor', isCorrect: false },
+				{ answerText: 'Cualquier trabajo que pueda utilizar los multinúcleo de la CPU', isCorrect: false },
+				{ answerText: 'Al realizar múltiples solicitudes a diferentes endpoint de una API', isCorrect: false },
+				{ answerText: 'Todas las opciones', isCorrect: true },
 			],
 		},
 		{
-			questionText: 'How many Harry Potter books are there?',
+			questionText: '¿Con que metodo restamos en 1 los waitgroups?',
 			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
+				{ answerText: 'start()', isCorrect: false },
+				{ answerText: 'Deduct()', isCorrect: false },
+				{ answerText: 'Wait()', isCorrect: false },
+				{ answerText: 'Done()', isCorrect: true },
 			],
 		},
 	];
@@ -60,13 +59,13 @@ export default function App() {
 		<div className='app'>
 			{showScore ? (
 				<div className='score-section'>
-					You scored {score} out of {questions.length}
+					Tu puntuación {score} de {questions.length}
 				</div>
 			) : (
 				<>
 					<div className='question-section'>
 						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
+							<span>Pregunta {currentQuestion + 1}</span>/{questions.length}
 						</div>
 						<div className='question-text'>{questions[currentQuestion].questionText}</div>
 					</div>
